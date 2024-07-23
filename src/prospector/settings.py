a599624b8710d55157020ec2f81ce813ef8028ec
Adil-bh <62136456+Adil-bh@users.noreply.github.com>
2024-07-23 15:54:58 +0200

    Set Login and Logout redirect url to home 🖥️

--- src/prospector/settings.py
+++ src/prospector/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'prospector_app.CustomUser'
+
+LOGIN_REDIRECT_URL = 'prospector:home'
+LOGOUT_REDIRECT_URL = 'prospector:home'

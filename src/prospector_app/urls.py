92dd8337fd5033c52968cffa07e48786071acc29
Adil-bh <62136456+Adil-bh@users.noreply.github.com>
2024-07-23 15:57:02 +0200

    Urls views and templates for login and signup

--- src/prospector_app/urls.py
+++ src/prospector_app/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
-from prospector_app.views import home
-
-app_name = "prospector_app"
+from django.urls import path, include
+from prospector_app.views import home, signup
+import django.contrib.auth.urls
+app_name = "prospector"
 
 urlpatterns = [
-    path("", home, name="home")
+    path("", home, name="home"),
+    path("accounts/signup/", signup, name="signup"),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
--- src/prospector_app/views.py
+++ src/prospector_app/views.py
@@ -1,8 +1,28 @@
-from django.http import HttpResponse, request
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.views import View
+
+from prospector_app.forms import CustomSignupForm
+from prospector_app.models import CustomUser
 
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+
+def signup(request):
+    context = {}
+
+    if request.method == "POST":
+        form = CustomSignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Bienvenue !")
+        else:
+            context["errors"] = form.errors
+
+    form = CustomSignupForm()
+    context["form"] = form
+
+    return render(request, "accounts/signup.html", context=context)

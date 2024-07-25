e14ca7dba959fb2129ac3797fb995d007a92acbe
Adil-bh <62136456+Adil-bh@users.noreply.github.com>
2024-07-25 17:23:13 +0200

    Add AccountActivation and PasswordReset Views

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
+
+from accounts import views
 from accounts.views import home, signup
+from django.contrib.auth import views as auth_views
 import django.contrib.auth.urls
 app_name = "accounts"
 
@@ -7,4 +10,11 @@ urlpatterns = [
     path("", home, name="home"),
     path("accounts/signup/", signup, name="signup"),
     path("accounts/", include("django.contrib.auth.urls")),
+    path('activate/<uidb64>/<token>', views.activate, name='activate'),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="registration/password_reset.html", email_template_name="registration/password_reset_email.html"), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_done.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_complete.html"), name="password_reset_complete"),
+
 ]

5f803e824740cd469d30e681ad90785e027c2624
Adil-bh <62136456+Adil-bh@users.noreply.github.com>
2024-08-13 14:00:06 +0200

    Views for Dashboard/Leads/Offers

--- src/tracking/views.py
+++ src/tracking/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from tracking.models import Leads, Offers
+
 
 # Create your views here.
+
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+
+class LeadsListView(ListView):
+    template_name = "leads.html"
+    model = Leads
+
+class OffersListView(ListView):
+    template_name = "offers.html"
+    model = Offers

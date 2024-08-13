e27b3632ba45e52800928377fd3cab15b790c2f8
Adil-bh <62136456+Adil-bh@users.noreply.github.com>
2024-08-13 13:59:29 +0200

    Add urls for tracking views

--- src/tracking/urls.py
+++ src/tracking/urls.py
@@ -1,3 +1,11 @@
+from django.urls import path
+
+from tracking.views import dashboard, LeadsListView, OffersListView
+
 app_name = "tracking"
 
-url_patterns = []
+urlpatterns = [
+    path("tracking/", dashboard, name="dashboard"),
+    path("tracking/leads/", LeadsListView.as_view(), name="leads"),
+    path("tracking/offers/", OffersListView.as_view(), name="offers")
+]
